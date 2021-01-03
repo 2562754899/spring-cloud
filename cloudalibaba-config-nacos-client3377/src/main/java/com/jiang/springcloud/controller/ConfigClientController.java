45bfee85b140ce072063011392f037d89b34e449
jiang <2562754899@qq.com>
2021-01-03 20:13:16 +0800

    案例sentinel限流测试

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/jiang/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.jiang.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * GTR
+ * 2020/12/24
+ * com.jiang.springcloud.controller
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
